89d3e1712836296adfd675693a483cd831d12e7e
aajordan2 <aajordan2@csuchico.edu>
2026-09-02 11:41:50 -0700

    fix divide by zero error

--- main.cpp
+++ main.cpp
@@ -16,7 +16,9 @@ int main()
     cout << "Addition: " << x + y << endl;
     cout << "Subtraction: " << x - y << endl;
     cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
+    if(y != 0){
+        cout << "Division: " << x / y << endl;
+    }
     cout << "Remainder: " << x % y << endl;
     cout << "Square Root: " << sqrt(x) << endl;
     cout << "Square: " << pow(x, y) << endl;
